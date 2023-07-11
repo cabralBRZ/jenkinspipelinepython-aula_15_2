dcd4d24deb73b2bdeb29d262f5e95fde2d98e3f7
cabralBRZ <117690840+cabralBRZ@users.noreply.github.com>
2023-07-10 21:23:43 -0300

    Update settings.py1

--- condomaisvc/settings.py
+++ condomaisvc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=m@(!(3)!%hxu)x%%!oh49u!_*_fhee84uhw&@&sdh@)&d(9i#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
